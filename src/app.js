7a498f9fe1a7c3786f5261bcd7167c41ed2a544d
Santi <santiagogcg@gmail.com>
2024-08-04 13:24:25 +0200

    finalProject_Quiz_BE_rev3

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const { routerProps } = require("./routes/prop.routes.js")
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', "https://finalproject-quiz-fe.onrender.com"],
     credentials: true
 
 }))
